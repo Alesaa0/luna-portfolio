2d2ad503214b2a5ddc795fe2cb37250ccb06d030
ZHUOYUE <372834545@qq.com>
2024-04-10 16:43:42 +0800

    first commit

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript and React/Vue these days.
+  I have worked on many types of projects (including B-end management systems, C-end platforms). Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/Alesaa0" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Software Engineering degree from Peking University. I hung it on the wall because I'm proud of it. `,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube to learn new things.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
+  Contact me at lunazy1999@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. `,
+  library: `There are a lot of programming books on my shelves.
+  What programmer doesn't have a 黑皮书 on their shelf?`,
+  exit: `If you want to exit Luna's portfolio, just close the tab.`,
+};
